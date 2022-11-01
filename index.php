aadd58e18febeeaca4bc09825475c6903250f5b6
Min Pyae <86566662+Min-Pyae@users.noreply.github.com>
2022-11-01 16:47:18 +0630

    Update index.php line 24 and 25

--- index.php
+++ index.php
@@ -21,8 +21,8 @@
     <title>DRIVERLESS Home Page</title>
 
     <!-- BOOTSTRAP CSS -->
-    <link rel="stylesheet" href="./vendor/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="./vendor/bootstrap/icons/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="./vendor/bootstrap/css/bootstrap.min.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="./vendor/bootstrap/icons/font/bootstrap-icons.css?v=<?php echo time(); ?>">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 
 
